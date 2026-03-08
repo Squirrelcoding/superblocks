763898a95c519f72086d4a46578e83a77db5eb34
Squirrelcoding <rosilescarlos28@gmail.com>
2026-03-07 18:41:31 -0600

    first commit

---
+++ src/main/java/name/modid/Superblocks.java
@@ -0,0 +1,23 @@
+package name.modid;
+
+import net.fabricmc.api.ModInitializer;
+
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.world.item.CreativeModeTabs;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Superblocks implements ModInitializer {
+	public static final String MOD_ID = "superblocks";
+
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod id as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+
+	@Override
+	public void onInitialize() {
+		ModItems.initialize();
+	}
+}
